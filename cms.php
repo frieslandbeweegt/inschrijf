d2078d86d0ce566bdd311b1430423b53baeaeea4
frieslandbeweegt <44971803+frieslandbeweegt@users.noreply.github.com>
2018-11-28 13:39:46 +0100

    Add files via upload

--- cms.php
+++ cms.php
@@ -32,24 +32,30 @@ include( 'server.php' );
 					<div class="main-event">
 						<div class="evenementen">
 							<div class="row" style="margin-bottom: 10px;">
-								<div class="col-md-2">
-									<div class="btn btn-danger minEvent">
-										<i class="fas fa-minus"></i>
+								<div class="col-md-12">
+									<input type="text" class="form-control" id="evenement" aria-describedby="evenement" name="evenement" placeholder="Evenement" required>
+									<div class="row">
+										<div class="col-md-6">
+										<h6>Begin Datum:</h6>
+										<input id="datum_begin" type="text" class="form-control" name="datum_begin" placeholder="begin datum" required>
+										</div>
+										<div class="col-md-6">
+										<h6>Eind Datum:</h6>
+										<input id="datum_eind" type="text" class="form-control" name="datum_eind" placeholder="eind datum" required>
+										</div>
 									</div>
 								</div>
-								<div class="col-md-10">
-									<input type="text" class="form-control" id="evenement" aria-describedby="evenement" name="evenement" placeholder="Evenement">
-								</div>
+								
 
 								<div class="col-md-6">
 									<div class="form-check">
-										<input class="form-check-input inlineCheckbox1" type="checkbox" value="Extra mensen" onClick="showInput()">
+										<input class="form-check-input inlineCheckbox1" type="checkbox" value="Extra mensen" name="check1" onClick="showInput()">
 										<label class="form-check-label" for="inlineCheckbox1">Extra mensen</label>
-										<input type="number" class="form-control max_extra" placeholder="max extra deelnemers" style="display: none;">
+										<input type="number" class="form-control max_extra" placeholder="max extra deelnemers" name="extra_inschrijven" style="display: none;">
 									</div>
 
 									<div class="form-check">
-										<input class="form-check-input inlineCheckbox2" type="checkbox" value="Vervoer" onClick="showInput2();">
+										<input class="form-check-input inlineCheckbox2" type="checkbox" value="Vervoer" onClick="showInput2();" name="check2">
 										<label class="form-check-label" for="inlineCheckbox2">Vervoer</label>
 
 										<div class="vervoer_zelf" style="display: none;">
@@ -63,13 +69,13 @@ include( 'server.php' );
 																</div>
 															</div>
 															<div class="col-md-6">
-																<input type="text" class="form-control" placeholder="Vervoer zelf">
+																<input type="text" class="form-control" placeholder="Vervoer zelf" name="vervoer">
 															</div>
 															<div class="col-md-4 input-group">
 																<div class="input-group-prepend">
 																	<span class="input-group-text" id="basic-addon1">€</span>
 																</div>
-																<input type="text" class="form-control" placeholder="costs">
+																<input type="text" class="form-control" placeholder="costs" name="vervoer_costs">
 															</div>
 														</div>
 													</div>
@@ -84,12 +90,12 @@ include( 'server.php' );
 									</div>
 
 									<div class="form-check">
-										<input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="Vegetarisch">
+										<input class="form-check-input" type="checkbox" id="inlineCheckbox3" name="check3" value="Vegetarisch">
 										<label class="form-check-label" for="inlineCheckbox3">Vegetarich</label>
 									</div>
 
 									<div class="form-check">
-										<input class="form-check-input inlineCheckbox4" type="checkbox" value="Edities" onClick="showInput3();">
+										<input class="form-check-input inlineCheckbox4" type="checkbox" name="check4" value="Edities" onClick="showInput3();">
 										<label class="form-check-label" for="inlineCheckbox4">Edities</label>
 
 										<div class="editie" style="display: none;">
@@ -103,13 +109,13 @@ include( 'server.php' );
 																</div>
 															</div>
 															<div class="col-md-6">
-																<input type="text" class="form-control" placeholder="Editie">
+																<input type="text" class="form-control" placeholder="Editie" name="editie">
 															</div>
 															<div class="col-md-4 input-group">
 																<div class="input-group-prepend">
 																	<span class="input-group-text" id="basic-addon2">€</span>
 																</div>
-																<input type="text" class="form-control" placeholder="costs">
+																<input type="text" class="form-control" placeholder="costs" name="editie_costs">
 															</div>
 														</div>
 													</div>
@@ -125,7 +131,7 @@ include( 'server.php' );
 								</div>
 								<div class="col-md-6">
 									<div class="form-check">
-										<input class="form-check-input inlineCheckbox5" type="checkbox" value="Accomodatie" onClick="showInput4();">
+										<input class="form-check-input inlineCheckbox5" type="checkbox" value="Accomodatie" name="check5" onClick="showInput4();">
 										<label class="form-check-label" for="inlineCheckbox5">Accomodatie</label>
 
 										<div class="accomodatie" style="display: none;">
@@ -139,13 +145,13 @@ include( 'server.php' );
 																</div>
 															</div>
 															<div class="col-md-6">
-																<input type="text" class="form-control" placeholder="Accomodatie">
+																<input type="text" class="form-control" placeholder="Accomodatie" name="accomodatie">
 															</div>
 															<div class="col-md-4 input-group">
 																<div class="input-group-prepend">
 																	<span class="input-group-text" id="basic-addon3">€</span>
 																</div>
-																<input type="text" class="form-control" placeholder="costs">
+																<input type="text" class="form-control" placeholder="costs" name="accomodatie_costs">
 															</div>
 														</div>
 													</div>
@@ -160,12 +166,12 @@ include( 'server.php' );
 									</div>
 
 									<div class="form-check">
-										<input class="form-check-input" type="checkbox" id="inlineCheckbox6" value="Annuleringsverzekering">
+										<input class="form-check-input" type="checkbox" id="inlineCheckbox6" name="check6" value="Annuleringsverzekering">
 										<label class="form-check-label" for="inlineCheckbox6">Annuleringsverzekering</label>
 									</div>
 
 									<div class="form-check">
-										<input class="form-check-input inlineCheckbox7" type="checkbox" value="Verhuur" onClick="showInput5();">
+										<input class="form-check-input inlineCheckbox7" type="checkbox" name="check7" value="Verhuur" onClick="showInput5();">
 										<label class="form-check-label" for="inlineCheckbox7">Verhuur</label>
 										
 										<div class="verhuur" style="display: none;">
@@ -179,13 +185,13 @@ include( 'server.php' );
 																</div>
 															</div>
 															<div class="col-md-6">
-																<input type="text" class="form-control" placeholder="Verhuur">
+																<input type="text" class="form-control" placeholder="Verhuur" name="verhuur">
 															</div>
 															<div class="col-md-4 input-group">
 																<div class="input-group-prepend">
 																	<span class="input-group-text" id="basic-addon3">€</span>
 																</div>
-																<input type="text" class="form-control" placeholder="costs">
+																<input type="text" class="form-control" placeholder="costs" name="verhuur_costs">
 															</div>
 														</div>
 													</div>
@@ -201,16 +207,11 @@ include( 'server.php' );
 								</div>
 								<div class="col-md-4">
 									<h6>Extra</h6>
-									<input type="text" class="form-control" id="Prijs" aria-describedby="prijs" placeholder="Prijs">
-									<input type="number" class="form-control" id="max_deelname" placeholder="Max deelname" style="margin-top: 10px;">
+									<input type="text" class="form-control" id="Prijs" aria-describedby="prijs" placeholder="Prijs" name="prijs" required>
+									<input type="number" class="form-control" id="max_deelname" placeholder="Max deelname" style="margin-top: 10px;" name="max_deelnemers" required>
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>
-				<div class="col-md-1">
-					<div class="btn btn-primary addEvent">
-						<i class="fas fa-plus"></i>
 					</div>
 				</div>
 			</div>
